30ae975a7b82bdb002f029ca88c023e14297ca3b
Yuri <yuri.cabrera.riv@gmail.com>
2025-11-01 18:41:13 +0100

    saturday changes

--- vite-vanilla-boilerplate/src/main.js
+++ vite-vanilla-boilerplate/src/main.js
@@ -1,42 +1,55 @@
+// FETCHING IMAGES
 import "./style.css";
 
-const container = document.getElementById("content");
+const container = document.getElementById("gallery");
 
+let currentPage = 1;
+const limit = 10;
+
+// Create image element and add to container //
 function createImage(src) {
-  const image = document.createElement("img");
+  const image = document.createElement('img');
   image.src = src;
+  image.style.width = "432px";
+  image.style.marginBottom = "1rem";
+  image.style.gap = "1rem";
+  image.style.borderRadius = "16px";
   container.appendChild(image);
 }
 
-function init() {
-  fetch(
-    "https://image-feed-api.vercel.app/api/images/e8cd3ffd-794c-4ec6-b375-7788dbb14275"
-  )
-    .then((resp) => resp.json())
-    .then((json) => createImage(json.image_url));
+// Find images from API //
+async function fetchImages(page) {
+  try {
+    const resp = await fetch(`https://image-feed-api.vercel.app/api/images?page=${page}`);
+    const json = await resp.json();
+
+    json.data.forEach(img => createImage(img.image_url));
+
+  } catch (error) {
+    console.error("Error to find images:", error);
+  }
 }
 
-init();
+// Charge initial images //
+fetchImages(currentPage);
 
 
-// MY SCRIPTS
+// YURI SCRIPTS >>
 
+// LOGO BUTTON FOR IMG LOADER
+const button = document.getElementById('logoImgLoader-btn');
+const textSection = document.querySelector('.welcomingCopy');
 
-// CHECKBOX FOR DARK-LIGHT MODE
-/*const switchBtn = getElementById('switch');
-const mode = document.querySelector('.mode')
+button.addEventListener("click", () => {
+  const showingText = !textSection.classList.contains('hidden');
 
-switchBtn.addEventListener('change', () => {
-
-  if(switchBtn.checked) {
-    document.body.classList.add('dark');
-    mode.innerHTML='DARK'
+  if(showingText) {
+    textSection.classList.add('hidden');
+    container.style.display = "inline-flex";
   } else {
-      document.body.classList.remove('dark');
-      mode.innerHTML='LIGHT'
-  }
-})*/
-
+    container.style.display = "inline-flex";
+  };
+})
 
 
 // BUTTON FOR DARK-LIGHT MODE
